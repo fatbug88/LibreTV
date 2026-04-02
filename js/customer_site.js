552f6fecdf8e437fc8f8d8699c2f1b5d05c65c1d
fatbug88 <72292068+fatbug88@users.noreply.github.com>
2026-04-02 20:41:36 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,11 +2,9 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
-    }
-   ffzy: {
         api: 'http://api.ffzyapi.com/api.php/provide/vod',
         name: '非凡综合资源',
-    } 
+    }
 };
 
 // 调用全局方法合并
